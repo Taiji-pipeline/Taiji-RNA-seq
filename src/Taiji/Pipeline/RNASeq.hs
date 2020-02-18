e92cb6ce074f612f6152f6970a20f0acebe9c326
Kai Zhang <kai@kzhang.org>
2020-02-18 15:23:36 -0800

    minor

--- src/Taiji/Pipeline/RNASeq.hs
+++ src/Taiji/Pipeline/RNASeq.hs
@@ -4,39 +4,25 @@
 {-# LANGUAGE TemplateHaskell   #-}
 
 module Taiji.Pipeline.RNASeq
-    ( inputReader
-    , builder
+    ( builder
     , RNASeqConfig(..)
     ) where
 
 import           Bio.Data.Experiment.Parser
 import           Data.Bifunctor                          (bimap)
-import qualified Data.Text                               as T
 import           Control.Workflow
-import qualified Data.IntMap.Strict as M
+import Bio.Data.Experiment.Types
 
 import           Taiji.Pipeline.RNASeq.Types
 import           Taiji.Pipeline.RNASeq.Functions
 import Taiji.Prelude
 
-inputReader :: String    -- ^ The key
-            -> Builder ()
-inputReader key = do
-    node "Read_Input" [| \_ -> do
-        input <- asks _rnaseq_input
-        es <- liftIO $ if ".tsv" == reverse (take 4 $ reverse input)
-            then readRNASeqTSV input key
-            else readRNASeq input key
-        forM_ es $ \e -> forM_ (M.toList $ e^.replicates) $ \(i, rep) ->
-            when (length (rep^.files) > 1) $ error $ printf
-                "replicate %d in \"%s\" contains more than 2 files"
-                i (T.unpack $ e^.eid)
-        return es
-        |] $ doc .= "Read RNA-seq data information from input file."
-    ["Read_Input"] ~> "Download_Data"
-
 builder :: Builder ()
 builder = do
+    node "Read_Input" [| \() -> do
+        input <- asks _rnaseq_input
+        liftIO $ mkInputReader input "RNA-seq" RNASeq
+        |] $ doc .= "Read input data information."
     nodePar "Download_Data" 'rnaDownloadData $ return ()
     uNode "Get_Fastq" 'rnaGetFastq
     node "Make_Index" 'rnaMkIndex $ memory .= 40
@@ -49,7 +35,7 @@ builder = do
         |] $ do
         nCore .= 4
         memory .= 40
-    path ["Download_Data", "Get_Fastq", "Make_Index", "Align",
+    path ["Read_Input", "Download_Data", "Get_Fastq", "Make_Index", "Align",
         "Quant"]
 
     node "Convert_ID_To_Name" [| \(ori, input) -> do
